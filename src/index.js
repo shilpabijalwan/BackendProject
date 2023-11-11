f5d777cab4a98f0f9936bc670172f7afb8146481
shilpa <shilpa@example.com>
2023-11-12 03:47:09 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("helo");
+import dotenv from "dotenv";
+
+import express from "express";
+import connectDB from "./db/index.js";
+
+const app = express();
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+// async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (err) => {
+//       console.log("Error connecting", err);
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log("app listening on port", process.env.PORT);
+//     });
+//   } catch (error) {
+//     console.error("error", error);
+//   }
+// };
